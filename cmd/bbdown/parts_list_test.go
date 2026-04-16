d2ba2d6b12c473193cdcd285a99ba307281422f1
Charles Zheng <charleszheng44@gmail.com>
2026-04-16 13:28:26 -0700

    test(cmd): use t.Run subtests for TestFormatDuration

--- cmd/bbdown/parts_list_test.go
+++ cmd/bbdown/parts_list_test.go
@@ -1,26 +1,31 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestFormatDuration(t *testing.T) {
 	cases := []struct {
+		name string
 		in   int
 		want string
 	}{
-		{-1, "--:--"},
-		{0, "--:--"},
-		{1, "00:01"},
-		{59, "00:59"},
-		{60, "01:00"},
-		{3599, "59:59"},
-		{3600, "01:00:00"},
-		{3661, "01:01:01"},
-		{36000, "10:00:00"},
+		{"negative", -1, "--:--"},
+		{"zero", 0, "--:--"},
+		{"one_second", 1, "00:01"},
+		{"fifty_nine_seconds", 59, "00:59"},
+		{"one_minute", 60, "01:00"},
+		{"just_under_one_hour", 3599, "59:59"},
+		{"one_hour", 3600, "01:00:00"},
+		{"over_one_hour", 3661, "01:01:01"},
+		{"ten_hours", 36000, "10:00:00"},
 	}
 	for _, tc := range cases {
-		got := formatDuration(tc.in)
-		if got != tc.want {
-			t.Errorf("formatDuration(%d) = %q, want %q", tc.in, got, tc.want)
-		}
+		t.Run(tc.name, func(t *testing.T) {
+			got := formatDuration(tc.in)
+			if got != tc.want {
+				t.Errorf("formatDuration(%d) = %q, want %q", tc.in, got, tc.want)
+			}
+		})
 	}
 }
